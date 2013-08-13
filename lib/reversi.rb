050d812059ba96bceed6874c303d2a8e2069f138
flada-auxv <aseknock@gmail.com>
2013-08-13 22:47:50 +0900

    add Reversi.new

--- lib/reversi.rb
+++ lib/reversi.rb
@@ -1,3 +1,16 @@
 class Reversi
+  attr_accessor :board
 
+  def initialize
+    @board = [
+      [nil,nil,nil,nil,nil,nil,nil,nil],
+      [nil,nil,nil,nil,nil,nil,nil,nil],
+      [nil,nil,nil,nil,nil,nil,nil,nil],
+      [nil,nil,nil,true,false,nil,nil,nil],
+      [nil,nil,nil,false,true,nil,nil,nil],
+      [nil,nil,nil,nil,nil,nil,nil,nil],
+      [nil,nil,nil,nil,nil,nil,nil,nil],
+      [nil,nil,nil,nil,nil,nil,nil,nil]
+    ]
+  end
 end
--- spec/reversi_spec.rb
+++ spec/reversi_spec.rb
@@ -1,5 +1,23 @@
 require 'spec_helper'
 
 describe 'Reversi' do
-  it { should be_true }
+  describe '#initialize' do
+    let(:reversi) { Reversi.new }
+    let(:board) {
+      [
+        [nil,nil,nil,nil,nil,nil,nil,nil],
+        [nil,nil,nil,nil,nil,nil,nil,nil],
+        [nil,nil,nil,nil,nil,nil,nil,nil],
+        [nil,nil,nil,true,false,nil,nil,nil],
+        [nil,nil,nil,false,true,nil,nil,nil],
+        [nil,nil,nil,nil,nil,nil,nil,nil],
+        [nil,nil,nil,nil,nil,nil,nil,nil],
+        [nil,nil,nil,nil,nil,nil,nil,nil]
+      ]
+    }
+
+    specify '黒と白のコマが交互に2枚ずつ置かれていること' do
+      expect(reversi.board).to eq board
+    end
+  end
 end
