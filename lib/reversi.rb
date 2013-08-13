a8eb10438b72958945b942b8016cfc0c238387f8
flada-auxv <aseknock@gmail.com>
2013-08-14 02:47:01 +0900

    ひっくり返すのを実装

--- lib/reversi.rb
+++ lib/reversi.rb
@@ -16,13 +16,57 @@ class Reversi
       [n,n,n,n,n,n,n,n],
       [n,n,n,n,n,n,n,n]
     ]
+
+    @directions = {
+      '1' => [-1, -1],
+      '2' => [-1,  0],
+      '3' => [-1, +1],
+      '4' => [ 0, -1],
+      '5' => [ 0,  0],
+      '6' => [ 0, +1],
+      '7' => [+1, -1],
+      '8' => [+1,  0],
+      '9' => [+1, +1]
+    }
+
+    @turn = b
   end
 
   def move_black(x, y)
+    reverse_candidates = check(x, y)
+    reverse!(reverse_candidates)
     @board[x][y] = true
   end
 
   def move_white(x, y)
+    reverse_candidates = check(x, y)
+    reverse!(reverse_candidates)
     @board[x][y] = false
+  end
+
+  def check(x, y)
+    @directions.each_with_object([]) do |(dir, (a, b)), candidate_pieces|
+      check_direction(x + a, y + b, dir, candidate_pieces)
+    end
+  end
+
+  private
+
+  # 対戦相手のピースかどうか
+  def opponent_piece?(piece)
+    piece != @turn && !piece.nil?
+  end
+
+  def check_direction(x, y, dir, candidate_pieces)
+    if opponent_piece?(@board[x][y])
+      candidate_pieces << [x, y]
+
+      a, b = @directions[dir]
+      check_direction(x + a, y + b, dir, candidate_pieces)
+    end
+  end
+
+  def reverse!(pieces)
+    pieces.each {|x, y| @board[x][y] = @turn }
   end
 end
--- spec/reversi_spec.rb
+++ spec/reversi_spec.rb
@@ -9,16 +9,15 @@ describe 'Reversi' do
     let(:n) { nil }
     let(:board) {
       [
-        #a b c d e f g h
         #0 1 2 3 4 5 6 7
-        [n,n,n,n,n,n,n,n], #1 0
-        [n,n,n,n,n,n,n,n], #2 1
-        [n,n,n,n,n,n,n,n], #3 2
-        [n,n,n,w,b,n,n,n], #4 3
-        [n,n,n,b,w,n,n,n], #5 4
-        [n,n,n,n,n,n,n,n], #6 5
-        [n,n,n,n,n,n,n,n], #7 6
-        [n,n,n,n,n,n,n,n]  #8 7
+        [n,n,n,n,n,n,n,n], #0
+        [n,n,n,n,n,n,n,n], #1
+        [n,n,n,n,n,n,n,n], #2
+        [n,n,n,w,b,n,n,n], #3
+        [n,n,n,b,w,n,n,n], #4
+        [n,n,n,n,n,n,n,n], #5
+        [n,n,n,n,n,n,n,n], #6
+        [n,n,n,n,n,n,n,n]  #7
       ]
     }
 
@@ -29,21 +28,103 @@ describe 'Reversi' do
 
   describe '#move_black' do
     before do
-      reversi.move_black(5, 5)
+      reversi.move_black(4, 5)
     end
 
     specify '引数の座標に黒石が置かれること' do
-      expect(reversi.board[5][5]).to eq true
+      expect(reversi.board[4][5]).to eq true
     end
   end
 
   describe '#move_white' do
     before do
-      reversi.move_white(5, 5)
+      reversi.move_white(4, 5)
     end
 
     specify '引数の座標に白石が置かれること' do
-      expect(reversi.board[5][5]).to eq false
+      expect(reversi.board[4][5]).to eq false
+    end
+  end
+
+  describe '#check' do
+    let(:b) { true }
+    let(:w) { false }
+    let(:n) { nil }
+    let(:board) {
+      [
+        #0 1 2 3 4 5 6 7
+        [n,n,n,n,n,n,n,n], #0
+        [n,n,n,n,n,n,n,n], #1
+        [n,n,n,n,n,n,n,n], #2
+        [n,n,n,w,b,n,n,n], #3
+        [n,n,n,b,w,n,n,n], #4
+        [n,n,n,n,n,n,n,n], #5
+        [n,n,n,n,n,n,n,n], #6
+        [n,n,n,n,n,n,n,n]  #7
+      ]
+    }
+
+    context 'ゲームスタート直後の場合' do
+      context '横方向に相手の石を挟んだとき' do
+        before do
+          reversi.move_black(4, 5)
+        end
+
+        specify '挟んだ石がひっくり返されること' do
+
+          expect(reversi.board[4][4]).to eq true
+        end
+      end
+
+      context '縦方向に相手の石を挟んだとき' do
+        before do
+          reversi.move_black(2, 3)
+        end
+
+        specify '挟んだ石がひっくり返されること' do
+          expect(reversi.board[3][3]).to eq true
+        end
+      end
+    end
+
+    context '2巡目の場合' do
+      before do
+        reversi.move_black(2, 3)
+        reversi.move_white(2, 4)
+
+        # [
+        #   #0 1 2 3 4 5 6 7
+        #   [n,n,n,n,n,n,n,n], #0
+        #   [n,n,n,n,n,n,n,n], #1
+        #   [n,n,n,b,w,n,n,n], #2
+        #   [n,n,n,b,w,n,n,n], #3
+        #   [n,n,n,b,w,n,n,n], #4
+        #   [n,n,n,n,n,n,n,n], #5
+        #   [n,n,n,n,n,n,n,n], #6
+        #   [n,n,n,n,n,n,n,n]  #7
+        # ]
+      end
+
+      context '斜め方向に相手の石を挟んだとき' do
+        before do
+          reversi.move_black(1, 5)
+        end
+
+        specify '挟んだ石がひっくり返されること' do
+          expect(reversi.board[2][4]).to eq true
+        end
+      end
+
+      context '複数の相手の石を挟んだとき' do
+        before do
+          reversi.move_black(2, 5)
+        end
+
+        specify 'すべての挟んだ石がひっくり返されること' do
+          expect(reversi.board[2][4]).to eq true
+          expect(reversi.board[3][4]).to eq true
+        end
+      end
     end
   end
 end
