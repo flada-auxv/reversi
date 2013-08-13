da730ba535b9748dd9492f2200bbaab6e9720c9b
flada-auxv <aseknock@gmail.com>
2013-08-13 23:17:11 +0900

    readable

--- spec/reversi_spec.rb
+++ spec/reversi_spec.rb
@@ -22,8 +22,7 @@ describe 'Reversi' do
       ]
     }
 
-    # TODO コマ・石
-    specify '黒と白のコマが交互に2枚ずつ置かれていること' do
+    specify '黒と白の石が交互に2枚ずつ置かれていること' do
       expect(reversi.board).to eq board
     end
   end
