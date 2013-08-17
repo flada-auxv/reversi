fd3d1ba9e034a4f5f5bab1484e307947cd302143
flada-auxv <aseknock@gmail.com>
2013-08-17 23:39:49 +0900

    remove waste

--- lib/reversi/piece.rb
+++ lib/reversi/piece.rb
@@ -17,13 +17,5 @@ module Reversi
     def coordinates
       [x, y]
     end
-
-
-    private
-
-    def index_for(coordinate_str)
-      return coordinate_str[1].to_i - 1, coordinate_str[0].ord - 'a'.ord
-    end
-
   end
 end
