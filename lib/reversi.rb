0521d621399a08fa7f24fa281a09d0fbee3aa854
flada-auxv <aseknock@gmail.com>
2013-08-31 22:58:10 +0900

    Reversi::Game の #game_loop に実行ファイルの機能を移した テストは後で書く・・・はず
    例外で遷移してるところをなんとかしないと、ユーザ と AI の状態遷移でコードの共有できなくてつらくなりそうな気がしてきている

--- lib/reversi.rb
+++ lib/reversi.rb
@@ -1,17 +1,87 @@
 require_relative 'reversi/piece'
 require_relative 'reversi/board'
 
+Player = Struct.new(:color, :type) {
+  %w(ai user).each do |_type|
+    define_method("#{_type}?") do
+      type == _type.to_sym
+    end
+  end
+}
+
 module Reversi
   class Game
     class IllegalMovementError < StandardError; end
+    class SkipException < StandardError; end
+    class ExitException < StandardError; end
 
     def initialize
       @board = Reversi::Board.new
 
-      @turn = [:black, :white].cycle
+      @turn = [Player.new(:black, :user), Player.new(:white, :user)].cycle
 
       @reversible_pieces = []
     end
+
+    def game_loop
+      loop do
+        if @turn.peek.user?
+          print_board(board.search_movable_pieces_for(current_turn_color))
+
+          begin
+            input = read_user_input
+
+          # FIXME 例外で遷移するのではなく、内部の振る舞いとしてもてるはず!!
+          rescue SkipException
+            turn_change
+            redo
+          rescue ExitException
+            break
+          end
+        elsif @turn.peek.ai?
+          input = ai(self)
+        end
+
+        redo if input.nil?
+
+        begin
+          move(input)
+        rescue IllegalMovementError
+          help
+          redo
+        end
+        # check_game_end
+      end
+    end
+
+    def print_board(movable_pieces)
+      puts "turn -> #{current_turn_color}"
+      puts "------------------"
+      puts "  a b c d e f g h"
+
+      sio = StringIO.new
+
+      board.each_with_index do |piece, i|
+        x_idx = i % Reversi::Board::BOARD_SIZE
+        lineno = (i / Reversi::Board::BOARD_SIZE) + 1
+
+        sio << lineno if x_idx == 0
+
+        sio << '|'
+        case piece.color
+        when :none; movable_pieces.map(&:location).include?(piece.location) ? sio << '○' : sio << ' '
+        when :black; sio << "\e[32mb\e[m"
+        when :white; sio << "\e[33mw\e[m"
+        end
+
+        sio << "|\n" if x_idx == 7
+      end
+
+      puts sio.string
+      puts ''
+    end
+
+    INPUT_FORMAT = /[a-h][1-8]/
 
     def board(location = nil)
       return @board unless location
@@ -24,11 +94,11 @@ module Reversi
     end
 
     def current_turn_color
-      @turn.peek
+      @turn.peek.color
     end
 
     def turn_change
-      @turn.next
+      @turn.next.color
     end
 
     def move(location)
@@ -48,6 +118,30 @@ module Reversi
         next unless (next_piece = @board.next_piece_for(piece, dir))
         res << check_for_straight_line(next_piece, dir)
       }.compact.flatten(1) # XXX ちょっとつらい？
+    end
+
+    def read_user_input
+      print '>> '
+
+      case input = readline.chomp
+      when 'skip'
+        puts 'skipped!!'
+        raise SkipException
+      when 'exit','end'
+        puts 'exit!!'
+        print_score
+        raise ExitException
+      when 'score'
+        print_score
+        return nil
+      when ''
+        return nil
+      when INPUT_FORMAT
+        return input
+      else
+        help
+        return nil
+      end
     end
 
     private
@@ -71,6 +165,31 @@ module Reversi
     def reverse!
       @reversible_pieces.map(&:reverse)
       @reversible_pieces.clear
+    end
+
+    def help
+      puts <<-HELP
+
+❨╯#°□°❩╯<(そこには石を置く事ができません!!)
+❨╯°□°❩╯<(オセロは初めてかい？)
+
+『石の置き方』
+石を置く場所は、列・行の順に指定します 例）c3
+
+『便利なコマンド』
+skip:  石を置く場所が無い場合に、自分の手順を飛ばせます
+score: 現在のスコアを表示させます
+exit:  ゲームを終了させます ❨╯°□°❩╯︵┻━┻
+
+❨╯°□°❩╯<(もう一度だけチャンスを与えてやる)
+
+      HELP
+    end
+
+    def print_score
+      puts "------------------"
+      puts "black:#{score_of(:black)} -- white:#{score_of(:white)}"
+      puts ''
     end
   end
 end
